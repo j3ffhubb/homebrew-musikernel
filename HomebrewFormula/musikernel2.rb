7fed55fdbad3de70ed700670ad89676464313db2
MusiKernel Team <musikernel@nospam.org>
2015-11-26 19:09:33 -0800

    New release

--- HomebrewFormula/musikernel2.rb
+++ HomebrewFormula/musikernel2.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Musikernel2 < Formula
   homepage 'https://github.com/j3ffhubb/musikernel'
-  url 'https://github.com/j3ffhubb/musikernel/archive/musikernel2-15.12.0.zip'
-  sha1 '6ea80aa4bded20e5d2b7c037c80cdd8219e399c0'
+  url 'https://github.com/j3ffhubb/musikernel/archive/musikernel2-15.12.1.zip'
+  sha1 '81da95fa475b34705b23db628cc6944d57ddad05'
 
   depends_on 'python3'
   depends_on 'liblo'
