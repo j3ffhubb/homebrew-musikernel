0a05cbeffc8c43dd39694155db9a53f9f684d3c1
MusiKernel Team <musikernel@nospam.org>
2016-04-23 16:09:42 -0700

    Bump version

--- HomebrewFormula/musikernel2.rb
+++ HomebrewFormula/musikernel2.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Musikernel2 < Formula
   homepage 'https://github.com/j3ffhubb/musikernel'
-  url 'https://github.com/j3ffhubb/musikernel/archive/musikernel2-16.02.1.zip'
-  sha1 '2cee1c38a5ad1a27818038dad40490b0c9efc233'
+  url 'https://github.com/j3ffhubb/musikernel/archive/musikernel2-16.05.1.zip'
+  sha1 'f25ea1c5bf4e1e6c4ae4f3ef4927c255b194e241'
 
   depends_on 'python3'
   depends_on 'liblo'
