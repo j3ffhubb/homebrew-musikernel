06eb5418c9ec252843cb0dc6ad3c5c0a9a4a99fd
mk team <no-spam@no-spam.org>
2020-08-27 19:02:17 -0700

    Dependency fixes

--- HomebrewFormula/musikernel2.rb
+++ HomebrewFormula/musikernel2.rb
@@ -5,18 +5,17 @@ class Musikernel2 < Formula
   url 'https://github.com/j3ffhubb/musikernel/archive/musikernel2-16.05.7.zip'
   sha256 '867fd36adb488538620250cc5e040e3a24785e2884ba7bf57225d3c980dbaee5'
 
-  depends_on 'python3'
+  depends_on 'fftw'
+  depends_on 'j3ffhubb/musikernel/rubberband'
   depends_on 'liblo'
+  depends_on 'libsndfile'
+  depends_on 'numpy'
   depends_on 'portaudio'
   depends_on 'portmidi'
-  depends_on 'j3ffhubb/musikernel/rubberband'
-  depends_on 'fftw'
-  depends_on 'numpy'
   depends_on 'pyqt5'
-  depends_on 'libsndfile'
+  depends_on 'python3'
 
   def install
-    system "brew", "install", "numpy", "--with-python3"
     system "make", "-C", "src", "mac_osx"
     system "make", "-C", "src", "install_mac_osx"
   end
